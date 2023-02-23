f4940130cad45bf61bb5ca6c96efc119c74b20a2
AndreyShuma <0662389612a@gmail.com>
2023-02-23 17:39:14 +0200

    23-02-23

--- javaScript/exmpal.js
+++ javaScript/exmpal.js
@@ -1,12 +1,15 @@
-let a = [10, 25, 0, 2, 8, 7, 1, 0];
+let a = [10, 0, 5, 15, -8, 25];
 
-const sum = (arr, item) => {
-    let b = arr.indexOf(item);
-    console.log(b);
-    console.log(arr.splice(b, 1));
+// console.log(a.slice(2, 4));
+// console.log(a);
+
+
+let sum = (arr, item) => {
+    let index = arr.indexOf(item);
+
+    console.log(arr.splice(index, 1));
     return arr
 };
-
 
 // function sum(arr, elem) {
 //     const index = arr.indexOf(elem);
@@ -16,4 +19,35 @@ const sum = (arr, item) => {
 //     return arr;
 //   }
 
-console.log(sum(a, 0));
+// console.log(sum(a, 5));
+
+const purchase = {
+    Electronics: [
+        { name: 'Laptop', price: 1500 },
+        { name: 'Keybord', price: 100 },
+    ],
+    Textile: [
+        { name: 'Bag', price: 50 },
+    ]
+};
+
+const find = (items, name) => {
+    for ( let key in items) {
+        for( let item of items[key]) {
+            if (item.name === name) return item
+        }
+    }   
+};
+
+// console.log(find(purchase, 'Laptop')); 
+
+let arrTest = ['Andrey', 'Vova', 'Marina'];
+
+// function SetObject(name) {
+//     this.name = name;
+//     this.say = function() {return console.log('Hello!!')}
+// }
+
+// let h = new SetObject(arrTest[0]);
+// let g = new SetObject(arrTest[1]);
+// console.log(h, g.say());
--- javaScript/main.js
+++ javaScript/main.js
@@ -5,6 +5,7 @@ const input = document.querySelector('input[name="enterInput"]');
 const btnPush = document.querySelector('.btnPush');
 const btnClear = document.querySelector('.btnClear');
 const choice = document.querySelector('select[name="choice"]');
+const foto = document.querySelector('.foto');
 // const optionMy = document.querySelector('option[value="man"]');
 // console.log(document.forms);
 // console.log(choice);
@@ -17,13 +18,14 @@ const arrPush = () => {
     let html = `<div>${a}</div>`;
     result.push(html);
     input.value = '';
-    // a = '';
     return render.innerHTML = result.join('');
 }
 
 const arrClear = () => {
     result = [];
     input.value = '';
+    foto.innerHTML = '';
+    choice.value = null;
     return render.innerHTML = result;
 }
 
@@ -33,5 +35,10 @@ btnClear.addEventListener('click', arrClear );
 choice.addEventListener('change', () => {
     let c = `Hello ${choice.value}`
     console.log(c);
+    if( choice.value === 'man') {
+        foto.innerHTML = '<img src="./img/Man.jpg" width="80px">';
+    } else if (choice.value === 'woman') {
+        foto.innerHTML = '<img src="./img/Woman.jpg" width="80px">';
+    } else {foto.innerHTML = '' }
 });
 // console.dir(input);
